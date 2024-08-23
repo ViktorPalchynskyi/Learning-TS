8c4217ae19dbac86dd4b128910ff4c3a3f804b1a
Viktor <palchinskij19@gmail.com>
2024-08-23 20:25:27 +0300

    23.08.24 - type inference

--- Types/variables.ts
+++ Types/variables.ts
@@ -1,6 +1,5 @@
 // to compile need to set tsc globally by "npm install -g typescript" and use tsc variables.ts
 
-
 let moviewTitle: string = 'Amadeus';
 
 moviewTitle = 'Arrival';
@@ -9,3 +8,11 @@ let numCatLives: number = 9;
 numCatLives++;
 
 let gameOver: boolean = false;
+
+let tvShow = 'Olover Kitteridge';
+tvShow = 'The Ohter Two';
+tvShow = false;
+
+let isFunny = false;
+isFunny = true;
+isFunny = 22;
