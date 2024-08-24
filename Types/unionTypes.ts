cc25e5f8fe517ff8677e3f9db0a3f3f72459ef2b
Viktor <palchinskij19@gmail.com>
2024-08-24 12:25:54 +0300

    24.08.24 -  Type narrowing w/ union types

--- Types/unionTypes.ts
+++ Types/unionTypes.ts
@@ -14,3 +14,25 @@ type Loc = {
 
 let coordinates: Point | Loc = { x: 2, y: 44 };
 coordinates = { lat: 42, long: 55 };
+
+function printAge(age: number | string): void {
+    console.log(`You are ${age} years old.`);
+}
+
+function calculateTax(price: number | string, tax: number): number {
+    // if (typeof price === 'string') {
+    //     return ;
+    // } else {
+    //     return price * tax;
+    // }
+
+    if (typeof price === 'string') {
+        price = parseFloat(price);
+    }
+
+    return price * tax;
+
+    // return typeof price === 'string'
+    //     ? parseFloat(price) * tax
+    //     : price * tax;
+}
