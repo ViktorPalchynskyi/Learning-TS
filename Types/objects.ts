a42227ceee19cfaddbe88f95b91f576b684a60fd
Viktor <palchinskij19@gmail.com>
2024-08-24 10:58:19 +0300

    24.08.24 - Objects types v2

--- Types/objects.ts
+++ Types/objects.ts
@@ -3,3 +3,12 @@ function printName(person: { first: string; second: string }): void {
 }
 
 printName({ first: 'Viktor', second: 'Palchynskyi' });
+
+let coordiname: { x: number; y: number } = { x: 24, y: 2 };
+
+function randomCoordiname(): { x: number; y: number } {
+    return {
+        x: Math.round(Math.random() * 100),
+        y: Math.round(Math.random() * 100),
+    };
+}
