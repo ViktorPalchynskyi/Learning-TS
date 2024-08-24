4bdb7619e69e7275ab01e5a80e84b3440844ab35
Viktor <palchinskij19@gmail.com>
2024-08-24 14:12:41 +0300

    24.08.24 - TS watch mode

--- ConfiguringTS/index.js
+++ ConfiguringTS/index.js
@@ -1,5 +1,8 @@
-var norma = {
+"use strict";
+// to use the watch mode - tsc -w {filename} or tsc --watch {filename}
+const norma = {
     breed: 'Silkie',
     eggsPerWeek: 4,
     name: 'Norma',
+    age: 1,
 };
--- ConfiguringTS/index.ts
+++ ConfiguringTS/index.ts
@@ -1,12 +1,15 @@
+// to use the watch mode - tsc -w {filename} or tsc --watch {filename}
+
 interface Chiken {
     breed: string;
     eggsPerWeek: number;
     name: string;
+    age: number;
 }
 
 const norma: Chiken = {
     breed: 'Silkie',
     eggsPerWeek: 4,
     name: 'Norma',
+    age: 1,
 };
-
