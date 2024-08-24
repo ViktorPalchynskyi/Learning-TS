c41d0a6b2a9e5e44652f85d5473d091859b5607d
Viktor <palchinskij19@gmail.com>
2024-08-24 12:31:51 +0300

    24.08.24 -  Union types with arrays

--- Types/unionTypes.ts
+++ Types/unionTypes.ts
@@ -12,8 +12,8 @@ type Loc = {
     long: number;
 };
 
-let coordinates: Point | Loc = { x: 2, y: 44 };
-coordinates = { lat: 42, long: 55 };
+// let coordinates: Point | Loc = { x: 2, y: 44 };
+// coordinates = { lat: 42, long: 55 };
 
 function printAge(age: number | string): void {
     console.log(`You are ${age} years old.`);
@@ -36,3 +36,10 @@ function calculateTax(price: number | string, tax: number): number {
     //     ? parseFloat(price) * tax
     //     : price * tax;
 }
+
+// const nums: number[] | string[]= [123];
+const stuff: (number | string)[] = ['string', 242];
+
+const coordinates: (Point | Loc)[] = [];
+coordinates.push({ lat: 42, long: 55 });
+coordinates.push({ x: 42, y: 55 });
