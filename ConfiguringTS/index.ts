b120da3932980cd58265b944007ab59d9df7b10e
Viktor <palchinskij19@gmail.com>
2024-08-24 14:10:13 +0300

    24.08.24 - Init TS config

---
+++ ConfiguringTS/index.ts
@@ -0,0 +1,12 @@
+interface Chiken {
+    breed: string;
+    eggsPerWeek: number;
+    name: string;
+}
+
+const norma: Chiken = {
+    breed: 'Silkie',
+    eggsPerWeek: 4,
+    name: 'Norma',
+};
+
