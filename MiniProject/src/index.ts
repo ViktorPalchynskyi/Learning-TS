0c1edc3c1f81c89f3997149ae48d472561ac2fb9
Viktor <palchinskij19@gmail.com>
2024-08-25 19:42:11 +0300

    25.08.24 - added interface

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,3 +1,8 @@
+interface Todo {
+    text: string;
+    completed: boolean;
+}
+
 const btn = document.getElementById('btn') as HTMLButtonElement;
 const input = document.getElementById(
     'todoinput'
@@ -5,17 +10,27 @@ const input = document.getElementById(
 const form = document.getElementById('todoform') as HTMLFormElement;
 const list = document.getElementById('todolist') as HTMLUListElement;
 
+const todos: Todo[] = [];
+
 const handleSubmit = (e: SubmitEvent) => {
     e.preventDefault();
-    const newTodoText = input.value;
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false,
+    };
+    todos.push(newTodo);
+    craeteTodo(newTodo);
+
+    input.value = '';
+};
+
+function craeteTodo(todo: Todo): void {
     const newLI = document.createElement('li');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    newLI.append(newTodoText);
+    newLI.append(todo.text);
     newLI.append(checkbox);
     list.append(newLI);
-    
-    input.value = '';
-};
+}
 
 form.addEventListener('submit', handleSubmit);
