10bf502ab2acbcf3d9748f8a3225c3a28b172fd4
Viktor <palchinskij19@gmail.com>
2024-08-26 14:43:32 +0300

    26.08.24  - writing generic function

--- Generics/dist/index.js
+++ Generics/dist/index.js
@@ -8,3 +8,17 @@ const btn = document.querySelector('.btn');
 if (inputEl) {
     inputEl.value = 'Viktor';
 }
+function numberIdentity(item) {
+    return item;
+}
+function stringIdentity(item) {
+    return item;
+}
+function booleanIdentity(item) {
+    return item;
+}
+function identity(item) {
+    return item;
+}
+identity('string');
+identity(24);
--- Generics/src/index.ts
+++ Generics/src/index.ts
@@ -11,3 +11,22 @@ const btn = document.querySelector<HTMLButtonElement>('.btn');
 if (inputEl) {
     inputEl.value = 'Viktor';
 }
+
+function numberIdentity(item: number): number {
+    return item;
+}
+
+function stringIdentity(item: string): string {
+    return item;
+}
+
+function booleanIdentity(item: boolean): boolean {
+    return item;
+}
+
+function identity<T>(item: T): T {
+    return item;
+}
+
+identity<string>('string');
+identity<number>(24);
