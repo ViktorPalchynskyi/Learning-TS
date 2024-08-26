ca4392c24decd11d63eba7194a3a32d959377774
Viktor <palchinskij19@gmail.com>
2024-08-26 12:51:34 +0300

    26.08.24 - pievate fields

--- ClassesRecap/dist/index.js
+++ ClassesRecap/dist/index.js
@@ -1,10 +1,18 @@
 class Player {
-    score = 0;
-    numLives = 10;
+    #score = 0;
+    #numLives = 10;
 
     constructor(first, last) {
         this.first = first;
         this.last = last;
+    }
+
+    get score() {
+        return this.#score;
+    }
+
+    set score(value) {
+        this.#score = value; 
     }
 
     taunt() {
@@ -12,7 +20,7 @@ class Player {
     }
 
     loseLive() {
-        this.numLives -= 1;
+        this.#numLives -= 1;
     }
 }
 
@@ -23,6 +31,9 @@ console.log(player);
 
 const player2 = new Player('Ogurchik', 'Motroskin');
 
-player2.numLives = 20;
 player2.loseLive();
 console.log(player2);
+
+player.score = 10;
+
+console.log(player.score);
