67b63ec1cda787a1d1a4183240a77e7efb5444d0
Viktor <palchinskij19@gmail.com>
2024-08-26 14:29:48 +0300

    26.08.24  - intoducing generics

---
+++ Generics/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// function doThing(thing: number | string): number | string {
+// }
+const nums = [];
+const color = [];
---
+++ Generics/src/index.ts
@@ -0,0 +1,6 @@
+// function doThing(thing: number | string): number | string {
+    
+// }
+
+const nums: Array<number> = [];
+const color: Array<string> = [];
