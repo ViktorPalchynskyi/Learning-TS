22ba85cb913c15597694fdc8d3ef958d60972c20
Viktor <palchinskij19@gmail.com>
2024-08-26 20:30:09 +0300

    26.08.24  - React.FC

---
+++ react_ts/src/components/Greeter/Greeter.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Greeter(): JSX.Element {
+    return <h2>Hi</h2>;
+}
+
+const Greeter1: React.FC = () => (
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Debitis neque provident est in saepe illum nemo quam, eaque,
+        dolores temporibus eos aliquam corrupti commodi blanditiis
+        nisi quas minus laudantium vel?
+    </p>
+);
