ba934b6eedf5538517db29bcb260bbc3fb7cdbfe
Viktor <palchinskij19@gmail.com>
2024-08-26 15:39:52 +0300

    26.08.24  -truthiness guard

--- TypeNarrowing/dist/index.js
+++ TypeNarrowing/dist/index.js
@@ -5,3 +5,20 @@ function triple(value) {
     }
     return value.repeat(3);
 }
+const el = document.getElementById('idk');
+if (el) {
+    el.innerHTML = 'Hello, there';
+}
+else {
+    const p = document.createElement('p');
+    p.innerHTML = 'I was created by TS';
+    document.body.append(p);
+}
+const printLetter = (word) => {
+    if (word) {
+        for (const char of word) {
+            console.log(char);
+        }
+    }
+    console.log('YOU DID NOT PASS IN A WORD!');
+};
--- TypeNarrowing/src/index.ts
+++ TypeNarrowing/src/index.ts
@@ -5,3 +5,23 @@ function triple(value: number | string): number | string {
 
     return value.repeat(3);
 }
+
+const el = document.getElementById('idk');
+
+if (el) {
+    el.innerHTML = 'Hello, there';
+} else {
+    const p = document.createElement('p');
+    p.innerHTML = 'I was created by TS';
+    document.body.append(p);
+}
+
+const printLetter = (word?: string): void => {
+    if (word) {
+        for (const char of word) {
+            console.log(char);
+        }
+    }
+
+    console.log('YOU DID NOT PASS IN A WORD!');
+};
