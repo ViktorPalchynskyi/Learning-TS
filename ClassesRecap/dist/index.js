225ce68b81e3b40170f96384b2dcadb0ef6b965a
Viktor <palchinskij19@gmail.com>
2024-08-26 13:19:24 +0300

    26.08.24 -private modifier  in TS

--- ClassesRecap/dist/index.js
+++ ClassesRecap/dist/index.js
@@ -2,8 +2,10 @@
 class Player {
     constructor(first, last) {
         this.score = 0;
+        this.id = 'asdf2e1254123hj14212423';
         this.first = first;
         this.last = last;
     }
 }
 const player = new Player('Carl', 'Jonson');
+// player.id
--- ClassesRecap/src/index.ts
+++ ClassesRecap/src/index.ts
@@ -2,6 +2,7 @@ class Player {
     public readonly first: string;
     public readonly last: string;
     public score: number = 0;
+    private id: string = 'asdf2e1254123hj14212423';
 
     constructor(first: string, last: string) {
         this.first = first;
@@ -10,3 +11,5 @@ class Player {
 }
 
 const player = new Player('Carl', 'Jonson');
+
+// player.id
