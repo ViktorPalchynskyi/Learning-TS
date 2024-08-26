280ef9a49027b79f4740987e76ebcdbd12547896
Viktor <palchinskij19@gmail.com>
2024-08-26 17:53:48 +0300

    26.08.24  - working with axios types

--- ThirdPartyLibs/dist/index.js
+++ ThirdPartyLibs/dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+axios_1.default
+    .get('https://jsonplaceholder.typicode.com/users')
+    .then((res) => {
+    console.log(res.data);
+    const { data } = res;
+    console.log(data[0].company.catchPhrase);
+})
+    .catch((e) => console.log(`Error ${e}`));
--- ThirdPartyLibs/src/index.ts
+++ ThirdPartyLibs/src/index.ts
@@ -1,1 +1,32 @@
-console
+import axios from 'axios';
+
+interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: { lat: string; lng: string };
+    };
+    phone: string;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    };
+}
+
+axios
+    .get<User []>('https://jsonplaceholder.typicode.com/users')
+    .then((res) => {
+        console.log(res.data);
+        const { data } = res;
+
+        console.log(data[0].company.catchPhrase);
+    })
+    .catch((e) => console.log(`Error ${e}`));
