764b35992220b88c5ffcc34673bf1fa497ae4e3a
Viktor <palchinskij19@gmail.com>
2024-08-26 19:53:03 +0300

    26.08.24  - webpack dev server

--- Webpack_TS/src/index.ts
+++ Webpack_TS/src/index.ts
@@ -1,3 +1,6 @@
+// webpack intsll  npm install --save-dev webpack webpack-cli typescript ts-loader
+// webpack dev server npm install --save-dev webpack-dev-server
+
 import Dog from './Dog';
 import ShelterDog from './ShelterDog';
 import { add, divide, multiply } from './utils';
--- Webpack_TS/webpack.config.js
+++ Webpack_TS/webpack.config.js
@@ -16,8 +16,14 @@ module.exports = {
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './'),
+        },
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist',
     },
 };
