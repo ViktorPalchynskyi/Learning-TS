a094f6fea2033fcd57136b79f31c96d3aec0404e
Viktor <palchinskij19@gmail.com>
2024-08-26 13:11:23 +0300

    26.08.24 - annotation class in TS

--- ClassesRecap/dist/index.js
+++ ClassesRecap/dist/index.js
@@ -1,39 +1,7 @@
+"use strict";
 class Player {
-    #score = 0;
-    #numLives = 10;
-
     constructor(first, last) {
         this.first = first;
         this.last = last;
     }
-
-    get score() {
-        return this.#score;
-    }
-
-    set score(value) {
-        this.#score = value; 
-    }
-
-    taunt() {
-        console.log('BOOYAH!');
-    }
-
-    loseLive() {
-        this.#numLives -= 1;
-    }
 }
-
-const player = new Player('Murchik', 'Motroskin');
-player.taunt();
-
-console.log(player);
-
-const player2 = new Player('Ogurchik', 'Motroskin');
-
-player2.loseLive();
-console.log(player2);
-
-player.score = 10;
-
-console.log(player.score);
--- ClassesRecap/src/index.ts
+++ ClassesRecap/src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello there!');
+class Player {
+    first: string;
+    last: string;
+    constructor(first: string, last: string) {
+        this.first = first;
+        this.last = last;
+    }
+}
