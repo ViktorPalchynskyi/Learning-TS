ea9969ac73f9c29c5dbfaf7aa388bc6156a82e61
Viktor <palchinskij19@gmail.com>
2024-08-26 13:28:46 +0300

    26.08.24 -parameter properties shorhand

--- ClassesRecap/dist/index.js
+++ ClassesRecap/dist/index.js
@@ -1,10 +1,33 @@
 "use strict";
+// class Player {
+//     public readonly first: string;
+//     public readonly last: string;
+//     public score: number = 0;
+//     private id: string = 'asdf2e1254123hj14212423';
+//     constructor(first: string, last: string) {
+//         this.first = first;
+//         this.last = last;
+//     }
+//     private secretMethod(): string {
+//         return this.id;
+//     }
+//     public showInfo(): void {
+//         console.log(
+//             `${this.first} ${this.last} have score: ${
+//                 this.score
+//             } and have id: ${this.secretMethod()}`
+//         );
+//     }
+// }
 class Player {
-    constructor(first, last) {
-        this.score = 0;
-        this.id = 'asdf2e1254123hj14212423';
+    constructor(first, last, score = 0) {
         this.first = first;
         this.last = last;
+        this.score = score;
+        // public readonly first: string;
+        // public readonly last: string;
+        // public score: number = 0;
+        this.id = 'asdf2e1254123hj14212423';
     }
     secretMethod() {
         return this.id;
--- ClassesRecap/src/index.ts
+++ ClassesRecap/src/index.ts
@@ -1,13 +1,38 @@
+// class Player {
+//     public readonly first: string;
+//     public readonly last: string;
+//     public score: number = 0;
+//     private id: string = 'asdf2e1254123hj14212423';
+
+//     constructor(first: string, last: string) {
+//         this.first = first;
+//         this.last = last;
+//     }
+
+//     private secretMethod(): string {
+//         return this.id;
+//     }
+
+//     public showInfo(): void {
+//         console.log(
+//             `${this.first} ${this.last} have score: ${
+//                 this.score
+//             } and have id: ${this.secretMethod()}`
+//         );
+//     }
+// }
+
 class Player {
-    public readonly first: string;
-    public readonly last: string;
-    public score: number = 0;
+    // public readonly first: string;
+    // public readonly last: string;
+    // public score: number = 0;
     private id: string = 'asdf2e1254123hj14212423';
 
-    constructor(first: string, last: string) {
-        this.first = first;
-        this.last = last;
-    }
+    constructor(
+        public readonly first: string,
+        public readonly last: string,
+        private score: number = 0,
+    ) {}
 
     private secretMethod(): string {
         return this.id;
