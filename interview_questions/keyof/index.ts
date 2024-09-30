61e01a23ed0ed5bb16758548999d3f91adf7d1ec
Viktor <palchinskij19@gmail.com>
2024-09-30 13:33:43 +0300

    30.09.24  - keyof

---
+++ interview_questions/keyof/index.ts
@@ -0,0 +1,22 @@
+namespace keyof {
+    interface User {
+        name: string;
+        id: string;
+        age: number;
+    }
+
+    type UserKeys = keyof User;
+
+    function getProperty<T extends object, K extends keyof T>(
+        obj: T,
+        key: K
+    ) {
+        return obj[key];
+    }
+
+    const user = { id: 1, name: 'John Doe', age: 30 };
+    const userName = getProperty(user, 'name'); // string
+    const userAge = getProperty(user, 'age'); // number
+
+    type UserType = typeof user;
+}
